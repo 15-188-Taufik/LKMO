d3174a5d283de5f2e9b46ec5ca2f4246b38e6b6e
lucyber99 <revolusialghifari@gmail.com>
2025-11-05 18:33:09 +0700

    perbaikan minor

--- app/admin/dahsboard/page.tsx
+++ app/admin/dahsboard/page.tsx
@@ -1,6 +1,7 @@
 import {Metadata} from "next";
 import DashboardCards from '@/app/components/admin/room/dashboard-cards'
 import { Suspense } from "react";
+import ReservationList from "@/app/components/admin/reservation-list";
 
 export const metadata: Metadata ={
     title: "Dashboard"
@@ -12,6 +13,9 @@ const DashboardPage = () => {
         <h1 className='text-4xl fond-bold text-gray-800'>Dashboard</h1>
         <Suspense fallback={<p>Loading cards...</p>}>
         <DashboardCards/>
+        </Suspense>
+        <Suspense fallback={<p>Loading...</p>}>
+        <ReservationList/>
         </Suspense>
     </div>
   )
--- app/components/payment-button.tsx
+++ app/components/payment-button.tsx
@@ -35,7 +35,7 @@ const PaymentButton = ({
 
   return (
     <button onClick={handlePayment} className='px-10 ppy-4 mt-2 text-center font-semibold text-white w-full bg-orange-400 rounded-sm hover:bg-orange-500 cursor-pointer'>
-        Process Payment
+       {isPending ? "Processing..." : "Process Payment"} 
     </button>
    )
 }
